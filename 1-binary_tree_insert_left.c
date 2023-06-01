b9f4b8935b5eaf677e5f40b72d6ae82d53198590
DannySanchez6658 <danieltsega6658@gmail.com>
2023-06-01 10:07:58 -0700

    [Fix]: Fixed the code by removing the create a node function by assigning it memory using malloc

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,10 +14,12 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 
+	binary_tree_t *newNode;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *newNode = binary_tree_node(parent, value);
+	newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)
 		return (NULL);
